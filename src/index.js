8666edc6738eda067f0091adc0aa2e6583da94f2
Protosevich <protosevich2001@gmail.com>
2020-03-11 17:13:55 +0300

    feat: sulution medium

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ function eval() {
     return;
 }
 
+String.prototype.splice = function(idx, rem, str) {
+    return this.slice(0, idx) + str + this.slice(rem);
+};
+
 const OPERATIONS = {
     '+' : sum,
     '-' : sub,
@@ -40,10 +44,37 @@ function delete_space(str){
     return str.replace(/ /g, '');
 }
 
+function check_brackets(str){
+    let open_br = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == '('){
+            open_br++;
+        }
+        if(str[i] == ')'){
+            open_br--;
+        }
+    }
+    if(open_br != 0) throw Error('ExpressionError: Brackets must be paired');
+}
+
 
 function expressionCalculator(expr) {
     expr = delete_space(expr);
+    check_brackets(expr);
+    expr = expr.replace(/\-\-/g, '+');
+    while (expr.indexOf('(') != -1){
+        let open_index_br = expr.lastIndexOf('(');
+        let close_index_br = expr.indexOf(')', open_index_br);
+        let result_in_br = String(expressionCalculator(expr.slice(open_index_br + 1, close_index_br)));
+        expr = expr.splice(open_index_br, close_index_br + 1, result_in_br);
+    }
     expr = expr.split('+').join(' +').split('-').join(' -').split(' ');
+    for(let i = 0; i < expr.length - 1; i++){
+        if(expr[i][expr[i].length - 1] === '*' || expr[i][expr[i].length - 1] === '/' || expr[i][expr[i].length - 1] === '+'){
+            expr[i] += expr[i + 1];
+            expr.splice(i+1, 1);
+        }
+    }
     for(let i = 0; i < expr.length; i++){
         let temp = expr[i].split('*').join(' *').split('/').join(' /').split(' ');
         while(temp.length > 1){
@@ -56,6 +87,10 @@ function expressionCalculator(expr) {
         expr[0] = simpleEval(expr[0]+expr[1]);
         expr.splice(1, 1);
     }
+    if(expr[0][0] == "+"){
+        expr[0] = expr[0].split('').slice(1).join('');
+    }
+
     return Number(expr[0]);
 }
 
@@ -63,3 +98,5 @@ module.exports = {
     expressionCalculator
 }
 
+// console.log('out:', expressionCalculator('3 * 26 / (  (  75 / 18 * 91 * 38  ) / 53 - (  52 / 34 - (  10 * 67 - 50 - 78  ) * 51 + 58  )  ) + 73 '));
+// console.log('out:', expressionCalculator('72*3/36*-18.571428571428573'));
